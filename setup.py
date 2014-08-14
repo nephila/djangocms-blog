b1eb97bc036f6bba247e876e8c05e618737148e9
Iacopo Spalletti <i.spalletti@nephila.it>
2014-08-14 08:53:49 +0200

    switch to beta classifier

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     zip_safe=False,
     keywords='djangocms-blog, blog, django, wordpress, multilingual',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
