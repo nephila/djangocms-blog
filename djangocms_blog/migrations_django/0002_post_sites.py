a5cbdcb8654efbf8e50cdf147bc8fc4095c50e76
Tadas Dailyda <tadas@dailyda.com>
2014-11-28 14:28:49 +0000

    migrations after 0.2 version

---
+++ djangocms_blog/migrations_django/0002_post_sites.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sites', '0001_initial'),
+        ('djangocms_blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='sites',
+            field=models.ManyToManyField(help_text='Select sites in which to show the post. If none is set it will bevisible in all the configured sites.', to='sites.Site', null=True, verbose_name='Site(s', blank=True),
+            preserve_default=True,
+        ),
+    ]
