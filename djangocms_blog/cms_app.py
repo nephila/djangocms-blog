74a78fc5a48ce834390590031d3d054214609ec0
Tadas Dailyda <tadas@dailyda.com>
2015-02-12 11:47:19 +0000

    Attach category menu to CMSApp

--- djangocms_blog/cms_app.py
+++ djangocms_blog/cms_app.py
@@ -1,9 +1,33 @@
 # -*- coding: utf-8 -*-
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
+from cms.menu_bases import CMSAttachMenu
+from menus.base import NavigationNode
+from menus.menu_pool import menu_pool
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _, get_language
-from .menu import BlogCategoryMenu
+from .models import BlogCategory
 
+
+class BlogCategoryMenu(CMSAttachMenu):
+    name = _('Blog Category menu')
+
+    def get_nodes(self, request):
+        nodes = []
+        qs = BlogCategory.objects.translated(get_language())
+        qs = qs.order_by('parent_id', 'translations__name').distinct()
+        for category in qs:
+            kwargs = { 'category': category.slug }
+            node = NavigationNode(
+                category.name,
+                reverse('djangocms_blog:posts-category', kwargs=kwargs),
+                category.pk,
+                category.parent_id
+            )
+            nodes.append(node)
+        return nodes
+
+menu_pool.register_menu(BlogCategoryMenu)
 
 class BlogApp(CMSApp):
     name = _('Blog')
