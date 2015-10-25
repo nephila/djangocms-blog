f029608f0cefd32e2cab01822323077e4c7cb56c
Iacopo Spalletti <i.spalletti@nephila.it>
2015-10-25 09:08:33 +0100

    Use djangocms-apphook-setup for auto setup

--- djangocms_blog/settings.py
+++ djangocms_blog/settings.py
@@ -79,6 +79,8 @@ def get_setting(name):
         'BLOG_AUTO_HOME_TITLE': getattr(settings, 'BLOG_AUTO_HOME_TITLE', 'Home'),
         'BLOG_AUTO_BLOG_TITLE': getattr(settings, 'BLOG_AUTO_BLOG_TITLE', 'Blog'),
         'BLOG_AUTO_APP_TITLE': getattr(settings, 'BLOG_AUTO_APP_TITLE', 'Blog'),
+        'BLOG_AUTO_NAMESPACE': getattr(settings, 'BLOG_AUTO_NAMESPACE', 'Blog'),
+
         'BLOG_ENABLE_SEARCH': getattr(settings, 'BLOG_ENABLE_SEARCH', True),
     }
     return default['BLOG_%s' % name]
