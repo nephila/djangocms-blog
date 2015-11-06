d7df39b6837ac837fecff15af0564c2b185e0d7e
Zoe Jin <zoe.jin@britny.se>
2015-11-06 10:32:57 +0100

    new unique together constraint- slug, sites, lanuguage_code for BlogCategory and Post model

--- djangocms_blog/models.py
+++ djangocms_blog/models.py
@@ -27,28 +27,31 @@ class BlogCategory(TranslatableModel):
     """
     Blog category
     """
-    parent = models.ForeignKey('self', verbose_name=_('parent'), null=True,
-                               blank=True)
+    parent = models.ForeignKey(
+        'self', verbose_name=_('parent'), null=True, blank=True)
     date_created = models.DateTimeField(_('created at'), auto_now_add=True)
     date_modified = models.DateTimeField(_('modified at'), auto_now=True)
 
     translations = TranslatedFields(
         name=models.CharField(_('name'), max_length=255),
         slug=models.SlugField(_('slug'), blank=True, db_index=True),
-        meta={'unique_together': (('language_code', 'slug'),)}
     )
 
-    sites = models.ManyToManyField('sites.Site', verbose_name=_(u'Site(s)'),
-                                   blank=True, null=True,
-                                   help_text=_(u'Select sites in which to show the categpry. '
-                                               u'If no site is set it will '
-                                               u'choose the current site.'))
+    sites = models.ManyToManyField(
+        'sites.Site',
+        verbose_name=_(u'Site(s)'),
+        blank=True,
+        null=True,
+        help_text=_(u'Select sites in which to show the categpry. '
+                    u'If no site is set it will '
+                    u'choose the current site.'))
 
     objects = TranslationManager()
 
     class Meta:
         verbose_name = _('blog category')
         verbose_name_plural = _('blog categories')
+        unique_together = ('slug', 'sites', 'language_code')
 
     @property
     def count(self):
@@ -59,7 +62,8 @@ class BlogCategory(TranslatableModel):
         if self.has_translation(lang):
             slug = self.safe_translation_getter(
                 'slug', language_code=lang, any_language=True)
-            return reverse('djangocms_blog:posts-category', kwargs={'category': slug})
+            return reverse('djangocms_blog:posts-category',
+                           kwargs={'category': slug})
         # in case category doesn't exist in this language, gracefully fallback
         # to posts-latest
         return reverse('djangocms_blog:posts-latest')
@@ -81,9 +85,10 @@ class Post(ModelMeta, TranslatableModel):
     """
     Blog post
     """
-    author = models.ForeignKey(dj_settings.AUTH_USER_MODEL,
-                               verbose_name=_(u'author'), null=True, blank=True,
-                               related_name='djangocms_blog_post_author')
+    author = models.ForeignKey(
+        dj_settings.AUTH_USER_MODEL,
+        verbose_name=_(u'author'), null=True, blank=True,
+        related_name='djangocms_blog_post_author')
 
     date_created = models.DateTimeField(_(u'created'), auto_now_add=True)
     date_modified = models.DateTimeField(_(u'last modified'), auto_now=True)
@@ -92,43 +97,53 @@ class Post(ModelMeta, TranslatableModel):
     date_published_end = models.DateTimeField(_(u'published Until'), null=True,
                                               blank=True)
     publish = models.BooleanField(_(u'publish'), default=False)
-    categories = models.ManyToManyField('djangocms_blog.BlogCategory', verbose_name=_(u'category'),
+    categories = models.ManyToManyField('djangocms_blog.BlogCategory',
+                                        verbose_name=_(u'category'),
                                         related_name='blog_posts',)
-    main_image = FilerImageField(verbose_name=_(u'main image'), blank=True, null=True,
+    main_image = FilerImageField(verbose_name=_(u'main image'),
+                                 blank=True, null=True,
                                  on_delete=models.SET_NULL,
                                  related_name='djangocms_blog_post_image')
-    main_image_thumbnail = models.ForeignKey('cmsplugin_filer_image.ThumbnailOption',
-                                             verbose_name=_(u'main image thumbnail'),
-                                             related_name='djangocms_blog_post_thumbnail',
-                                             on_delete=models.SET_NULL,
-                                             blank=True, null=True)
-    main_image_full = models.ForeignKey('cmsplugin_filer_image.ThumbnailOption',
-                                        verbose_name=_(u'main image full'),
-                                        related_name='djangocms_blog_post_full',
-                                        on_delete=models.SET_NULL,
-                                        blank=True, null=True)
-    enable_comments = models.BooleanField(verbose_name=_(u'enable comments on post'),
-                                          default=get_setting('ENABLE_COMMENTS'))
-    sites = models.ManyToManyField('sites.Site', verbose_name=_(u'Site(s)'), blank=True,
-                                   null=True,
-                                   help_text=_(u'Select sites in which to show the post. '
-                                               u'If no site is set it will '
-                                               u'choose the current site.'))
+    main_image_thumbnail = models.ForeignKey(
+        'cmsplugin_filer_image.ThumbnailOption',
+        verbose_name=_(u'main image thumbnail'),
+        related_name='djangocms_blog_post_thumbnail',
+        on_delete=models.SET_NULL,
+        blank=True, null=True)
+    main_image_full = models.ForeignKey(
+        'cmsplugin_filer_image.ThumbnailOption',
+        verbose_name=_(u'main image full'),
+        related_name='djangocms_blog_post_full',
+        on_delete=models.SET_NULL,
+        blank=True, null=True)
+    enable_comments = models.BooleanField(
+        verbose_name=_(u'enable comments on post'),
+        default=get_setting('ENABLE_COMMENTS'))
+    sites = models.ManyToManyField(
+        'sites.Site',
+        verbose_name=_(u'Site(s)'),
+        blank=True,
+        null=True,
+        help_text=_(u'Select sites in which to show the post. '
+                    u'If no site is set it will '
+                    u'choose the current site.'))
 
     translations = TranslatedFields(
         title=models.CharField(_(u'title'), max_length=255),
         slug=models.SlugField(_(u'slug'), blank=True, db_index=True),
         abstract=HTMLField(_(u'abstract'), blank=True, default=''),
-        meta_description=models.TextField(verbose_name=_(u'post meta description'),
+        meta_description=models.TextField(verbose_name=_(
+            u'post meta description'),
                                           blank=True, default=''),
         meta_keywords=models.TextField(verbose_name=_(u'post meta keywords'),
                                        blank=True, default=''),
-        meta_title=models.CharField(verbose_name=_(u'post meta title'),
-                                    help_text=_(u'used in title tag and social sharing'),
-                                    max_length=255,
-                                    blank=True, default=''),
+        meta_title=models.CharField(
+            verbose_name=_(u'post meta title'),
+            help_text=_(
+                u'used in title tag and social sharing'),
+            max_length=255,
+            blank=True, default=''),
         post_text=HTMLField(_(u'text'), default='', blank=True),
-        meta={'unique_together': (('language_code', 'slug'),)}
     )
     content = PlaceholderField('post_content', related_name='post_content')
 
@@ -167,6 +182,7 @@ class Post(ModelMeta, TranslatableModel):
         verbose_name_plural = _('blog articles')
         ordering = ('-date_published', '-date_created')
         get_latest_by = 'date_published'
+        unique_together = ('slug', 'sites', 'language_code')
 
     def __str__(self):
         return self.safe_translation_getter('title', any_language=True)
@@ -175,9 +191,8 @@ class Post(ModelMeta, TranslatableModel):
         kwargs = {'year': self.date_published.year,
                   'month': '%02d' % self.date_published.month,
                   'day': '%02d' % self.date_published.day,
-                  'slug': self.safe_translation_getter('slug',
-                                                       language_code=get_language(),
-                                                       any_language=True)}
+                  'slug': self.safe_translation_getter(
+                      'slug', language_code=get_language(), any_language=True)}
         return reverse('djangocms_blog:post-detail', kwargs=kwargs)
 
     def save(self, *args, **kwargs):
@@ -200,9 +215,11 @@ class Post(ModelMeta, TranslatableModel):
         return self.safe_translation_getter('meta_keywords').strip().split(',')
 
     def get_description(self):
-        description = self.safe_translation_getter('meta_description', any_language=True)
+        description = self.safe_translation_getter(
+            'meta_description', any_language=True)
         if not description:
-            description = self.safe_translation_getter('abstract', any_language=True)
+            description = self.safe_translation_getter(
+                'abstract', any_language=True)
         return escape(strip_tags(description)).strip()
 
     def get_image_full_url(self):
@@ -241,8 +258,10 @@ class BasePostPlugin(CMSPlugin):
 
     def post_queryset(self, request=None):
         language = get_language()
-        posts = Post._default_manager.active_translations(language_code=language)
-        if not request or not getattr(request, 'toolbar', False) or not request.toolbar.edit_mode:
+        posts = Post._default_manager.active_translations(
+            language_code=language)
+        if not request or not getattr(
+                request, 'toolbar', False) or not request.toolbar.edit_mode:
             posts = posts.published()
         return posts
 
@@ -252,13 +271,22 @@ class BasePostPlugin(CMSPlugin):
 
 class LatestPostsPlugin(BasePostPlugin):
 
-    latest_posts = models.IntegerField(_(u'articles'), default=get_setting('LATEST_POSTS'),
-                                       help_text=_(u'The number of latests articles to be displayed.'))
-    tags = TaggableManager(_(u'filter by tag'), blank=True,
-                           help_text=_(u'Show only the blog articles tagged with chosen tags.'),
-                           related_name='djangocms_blog_latest_post')
-    categories = models.ManyToManyField('djangocms_blog.BlogCategory', blank=True, verbose_name=_(u'filter by category'),
-                                        help_text=_(u'Show only the blog articles tagged with chosen categories.'))
+    latest_posts = models.IntegerField(
+        _(u'articles'),
+        default=get_setting('LATEST_POSTS'),
+        help_text=_(
+            u'The number of latests articles to be displayed.'))
+    tags = TaggableManager(
+        _(u'filter by tag'),
+        blank=True,
+        help_text=_(u'Show only the blog articles tagged with chosen tags.'),
+        related_name='djangocms_blog_latest_post')
+    categories = models.ManyToManyField(
+        'djangocms_blog.BlogCategory',
+        blank=True,
+        verbose_name=_(u'filter by category'),
+        help_text=_(
+            u'Show only the blog articles tagged with chosen categories.'))
 
     def __str__(self):
         return _(u'%s latest articles by tag') % self.latest_posts
@@ -300,5 +328,6 @@ class AuthorEntriesPlugin(BasePostPlugin):
         for author in authors:
             author.count = 0
             if author.djangocms_blog_post_author.filter(publish=True).exists():
-                author.count = author.djangocms_blog_post_author.filter(publish=True).count()
+                author.count = author.djangocms_blog_post_author.filter(
+                    publish=True).count()
         return authors
