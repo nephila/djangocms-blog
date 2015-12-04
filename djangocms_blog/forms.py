ac455392f0b4f41fe6d72f5fbb898b48f70c3a3c
Zoe Jin <zoe.jin@britny.se>
2015-12-04 10:50:19 +0100

    active on_site to categories filter in Latest Blog Articles plugin

--- djangocms_blog/forms.py
+++ djangocms_blog/forms.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.conf import settings
+from django.contrib.sites.models import Site
 from taggit_autosuggest.widgets import TagAutoSuggest
+
+from djangocms_blog.models import BlogCategory
 
 
 class LatestEntriesForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(LatestEntriesForm, self).__init__(*args, **kwargs)
         # self.fields['tags'].widget = TagAutoSuggest('taggit.Tag')
+        for field_name in self.fields:
+            if field_name == "categories":
+                formfield = self.fields[field_name]
+                if hasattr(formfield, 'queryset'):
+                    formfield.queryset = BlogCategory.objects.filter(
+                        sites=Site.objects.get_current())
 
     class Media:
         css = {
