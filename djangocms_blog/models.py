77f97ef98b0fa8bd9ed7adebc1250c7129885b06
Iacopo Spalletti <i.spalletti@nephila.it>
2016-04-08 21:34:54 +0200

    Fix code style

--- djangocms_blog/models.py
+++ djangocms_blog/models.py
@@ -8,8 +8,6 @@ from django.conf import settings as dj_settings
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 from django.utils import timezone
 from django.utils.encoding import force_text, python_2_unicode_compatible
 from django.utils.html import escape, strip_tags
