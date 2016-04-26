nephila/djangocms-blog

commit a1b2c5630528f0edb984888533775a737a612f83

Fix issues related to parler

djangocms_blog/views.py
@@ -16,10 +16,10 @@ class BaseBlogView(ViewUrlMixin):
 
     def get_queryset(self):
         language = get_language()
-        queryset = self.model._default_manager.active_translations(language_code=language)
+        queryset = self.model._default_manager.all().active_translations(language_code=language)
         if not getattr(self.request, 'toolbar', False) or not self.request.toolbar.edit_mode:
             queryset = queryset.published()
-        return queryset.on_site()
+        return queryset
 
     def render_to_response(self, context, **response_kwargs):
         response_kwargs['current_app'] = resolve(self.request.path).namespace
@@ -45,6 +45,12 @@ class PostDetailView(TranslatableSlugMixin, BaseBlogView, DetailView):
     template_name = 'djangocms_blog/post_detail.html'
     slug_field = 'slug'
     view_url_name = 'djangocms_blog:post-detail'
+
+    def get_queryset(self):
+        queryset = self.model._default_manager.all()
+        if not getattr(self.request, 'toolbar', False) or not self.request.toolbar.edit_mode:
+            queryset = queryset.published()
+        return queryset
 
     def get(self, *args, **kwargs):
         # submit object to cms to get corrent language switcher and selected category behavior

djangocms_blog/models.py
@@ -174,14 +174,9 @@ class Post(ModelMeta, TranslatableModel):
         return reverse('djangocms_blog:post-detail', kwargs=kwargs)
 
     def save(self, *args, **kwargs):
+        if not self.slug and self.title:
+            self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
-        main_lang = self.get_current_language()
-        for lang in self.get_available_languages():
-            self.set_current_language(lang)
-            if not self.slug and self.title:
-                self.slug = slugify(self.title)
-        self.set_current_language(main_lang)
-        self.save_translations()
 
     def get_title(self):
         title = self.safe_translation_getter('meta_title', any_language=True)

commit 20547c23e67405cc0db897bcd9a942ddf2f52d99

Only list posts from the current site
This fixes a regression introduced by a1b2c5630528f0edb984888533775a737a612f83
in which the `on_site()` filter from the blog posts list view was removed.

djangocms_blog/views.py
@@ -44,7 +44,7 @@ class BaseBlogView(AppConfigMixin, ViewUrlMixin):
         if not getattr(self.request, 'toolbar', False) or not self.request.toolbar.edit_mode:
             queryset = queryset.published()
         setattr(self.request, get_setting('CURRENT_NAMESPACE'), self.config)
-        return queryset
+        return queryset.on_site()
 
     def get_template_names(self):
         template_path = (self.config and self.config.template_prefix) or 'djangocms_blog'