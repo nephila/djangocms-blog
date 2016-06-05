238980a16b2847efb079cb44b951b832b43432b6
Iacopo Spalletti <i.spalletti@nephila.it>
2016-06-05 23:15:21 +0200

    Fix compatibility

--- tests/test_utils/urls.py
+++ tests/test_utils/urls.py
@@ -6,41 +6,41 @@ import sys
 from cms.sitemaps import CMSSitemap
 from cms.utils.conf import get_cms_setting
 from django.conf import settings
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.i18n import javascript_catalog
+from django.views.static import serve
 
 from djangocms_blog.sitemaps import BlogSitemap
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'^media/cms/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/cms/(?P<path>.*)$', serve,
         {'document_root': get_cms_setting('MEDIA_ROOT'), 'show_indexes': True}),
-    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+    url(r'^jsi18n/(?P<packages>\S+?)/$', javascript_catalog),
     url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+    url(r'^sitemap\.xml$', sitemap,
         {'sitemaps': {
             'cmspages': CMSSitemap, 'blog': BlogSitemap,
         }
-    }),
-)
+        }),
+]
 
 urlpatterns += staticfiles_urlpatterns()
 
 if 'server' not in sys.argv:
     urlpatterns += i18n_patterns(
-        '',
         url(r'^blog/', include(
             'djangocms_blog.urls', namespace='djangocms_blog', app_name='djangocms_blog'
         )),
     )
 urlpatterns += i18n_patterns(
-    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
