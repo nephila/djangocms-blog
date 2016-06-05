d19b469410a29c956b5e5667b97f24baa29e92b0
Iacopo Spalletti <i.spalletti@nephila.it>
2016-06-05 18:09:08 +0200

    Bump development version

--- djangocms_blog/__init__.py
+++ djangocms_blog/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Iacopo Spalletti'
 __email__ = 'i.spalletti@nephila.it'
-__version__ = '0.8.0'
+__version__ = '0.8.0.post1'
 
 default_app_config = 'djangocms_blog.apps.BlogAppConfig'
