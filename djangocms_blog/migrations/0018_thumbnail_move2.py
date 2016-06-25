1f484c625dd08f4de5d50cb474955848fb1c85db
Iacopo Spalletti <i.spalletti@nephila.it>
2016-06-25 17:10:28 +0200

    Moar migration fixes

--- djangocms_blog/migrations/0018_thumbnail_move2.py
+++ djangocms_blog/migrations/0018_thumbnail_move2.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     if 'cmsplugin_filer' not in thumbnail_model:
         dependencies = [
-            ('djangocms_blog', '0016_auto_20160502_1741'),
+            ('djangocms_blog', '0017_thumbnail_move'),
             ('filer', '0003_thumbnailoption'),
             ('cmsplugin_filer_image', '0003_mv_thumbnail_option_to_filer_20160119_1720'),
         ]
@@ -18,16 +18,16 @@ class Migration(migrations.Migration):
         ]
 
         operations = [
-            migrations.AlterField(
+            migrations.AddField(
                 model_name='post',
-                name='main_image_full',
+                name='main_image_full_new',
                 field=models.ForeignKey(related_name='djangocms_blog_post_full',
                                         verbose_name='Main image full', blank=True,
                                         to=thumbnail_model, null=True),
             ),
-            migrations.AlterField(
+            migrations.AddField(
                 model_name='post',
-                name='main_image_thumbnail',
+                name='main_image_thumbnail_new',
                 field=models.ForeignKey(related_name='djangocms_blog_post_thumbnail',
                                         verbose_name='Main image thumbnail', blank=True,
                                         to=thumbnail_model, null=True),
@@ -37,7 +37,7 @@ class Migration(migrations.Migration):
         dependencies = [
             ('filer', '__first__'),
             ('cmsplugin_filer_image', '__first__'),
-            ('djangocms_blog', '0016_auto_20160502_1741'),
+            ('djangocms_blog', '0017_thumbnail_move'),
         ]
 
         operations = []
