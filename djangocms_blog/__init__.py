9e7f66b3f5810f268bd39027bf21323bc6b7524e
Iacopo Spalletti <i.spalletti@nephila.it>
2016-10-25 09:14:28 +0200

    Release 0.8.9

--- djangocms_blog/__init__.py
+++ djangocms_blog/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Iacopo Spalletti'
 __email__ = 'i.spalletti@nephila.it'
-__version__ = '0.8.8'
+__version__ = '0.8.9'
 
 default_app_config = 'djangocms_blog.apps.BlogAppConfig'
