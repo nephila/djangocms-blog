485a9b45ad940382eed21402373c8c2e86b85c53
Iacopo Spalletti <i.spalletti@nephila.it>
2017-03-06 21:58:52 +0100

    Drop reverse migration dependency

--- djangocms_blog/migrations/0019_thumbnail_move3.py
+++ djangocms_blog/migrations/0019_thumbnail_move3.py
@@ -28,9 +28,6 @@ class Migration(migrations.Migration):
             ('filer', '0003_thumbnailoption'),
             ('cmsplugin_filer_image', '0003_mv_thumbnail_option_to_filer_20160119_1720'),
         ]
-        run_before = [
-            ('cmsplugin_filer_image', '0004_auto_20160120_0950'),
-        ]
 
         operations = [
             migrations.RunPython(move_thumbnail_opt_to_filer, move_thumbnail_opt_to_plugin),
