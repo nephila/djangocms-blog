4b53e94a979a64cc9312ccc5df1e8ca64fef0c9e
Dipen Patel <dipen@quixom.com>
2017-06-06 09:20:37 +0530

    migration issue fixed

--- djangocms_blog/migrations/0030_auto_20170509_1831.py
+++ djangocms_blog/migrations/0030_auto_20170509_1831.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangocms_blog', '0029_post_related.py'),
+        ('djangocms_blog', '0029_post_related'),
     ]
 
     operations = [
