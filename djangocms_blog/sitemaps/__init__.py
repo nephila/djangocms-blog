cd521a18a9a3dc53da088fe580a205d04066624a
Mike Liu <mike.liu.dev@gmail.com>
2017-06-14 23:35:36 -0400

    Store urls in a cache when iterating through items.
    These urls will be used in the location function so as to prevent calling
    `get_absolute_url()` twice.

--- djangocms_blog/sitemaps/__init__.py
+++ djangocms_blog/sitemaps/__init__.py
@@ -15,6 +15,10 @@ from ..settings import get_setting
 
 class BlogSitemap(Sitemap):
 
+    def __init__(self, *args, **kwargs):
+        super(BlogSitemap, self).__init__(*args, **kwargs)
+        self.url_cache = {}
+
     def priority(self, obj):
         if obj and obj.app_config:
             return obj.app_config.sitemap_priority
@@ -27,18 +31,21 @@ class BlogSitemap(Sitemap):
 
     def location(self, obj):
         with smart_override(obj.get_current_language()):
-            return obj.get_absolute_url(obj.get_current_language())
+            return self.url_cache[obj.get_current_language()][obj]
 
     def items(self):
         items = []
+        self.url_cache.clear()
         for lang in get_language_list():
+            self.url_cache[lang] = {}
             posts = Post.objects.translated(lang).language(lang).published()
             for post in posts:
                 # check if the post actually has a url before appending
                 # if a post is published but the associated app config is not
                 # then this post will not have a url
                 try:
-                    post.get_absolute_url()
+                    with smart_override(post.get_current_language()):
+                        self.url_cache[lang][post] = post.get_absolute_url()
                 except NoReverseMatch:
                     # couldn't determine the url of the post so pass on it
                     continue
