a0d95d7d67ad173016c714cfae970bcf3fac064c
Felipe Farias <felipelomba@gmail.com>
2017-10-15 20:03:52 -0300

    Not found author now returns a 404 error.

--- djangocms_blog/views.py
+++ djangocms_blog/views.py
@@ -15,6 +15,7 @@ from parler.views import TranslatableSlugMixin, ViewUrlMixin
 
 from .models import BlogCategory, Post
 from .settings import get_setting
+from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
@@ -165,7 +166,7 @@ class AuthorEntriesView(BaseBlogListView, ListView):
         return self.optimize(qs)
 
     def get_context_data(self, **kwargs):
-        kwargs['author'] = User.objects.get(**{User.USERNAME_FIELD: self.kwargs.get('username')})
+        kwargs['author'] = get_object_or_404(User, **{User.USERNAME_FIELD: self.kwargs.get('username')})
         context = super(AuthorEntriesView, self).get_context_data(**kwargs)
         return context
 
