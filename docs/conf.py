8d3ebf330f613a74a6b88ad1c6472357c419e9ca
Iacopo Spalletti <github@spalletti.it>
2019-03-05 21:28:02 +0100

    Fix sphinx conf isort (#481)

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,12 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
-import shlex
+import sys
+
+import sphinx.environment
+import sphinx_rtd_theme
+from docutils.utils import get_source_line
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -24,12 +27,10 @@ parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, parent)
 sys.path.insert(0, os.path.join(parent, 'tests'))
 
-import cms_helper
-import djangocms_blog
+import cms_helper  # isort:skip
+import djangocms_blog  # isort:skip
 cms_helper.setup()
 
-import sphinx.environment
-from docutils.utils import get_source_line
 
 
 def _warn_node(self, msg, node, *args, **kwargs):
@@ -129,9 +130,6 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
 
