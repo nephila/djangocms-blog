5063d89d211915874be1a55bc2cef29ecbd3ecfc
Iacopo Spalletti <i.spalletti@nephila.it>
2019-08-10 08:18:50 +0200

    Update README / classifiers [ci skip]

--- setup.py
+++ setup.py
@@ -60,10 +60,12 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
